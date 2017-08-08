cf24e2b76625c3c60d1be1216de8abd2c1f37dae
Mykola Gurov <ngurov@gmail.com>
2017-08-08 21:15:13 +0200

    cleanup

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 import AddWatchContainer from './components/AddWatchContainer.js'
 import WatchListContainer from './components/WatchListContainer.js'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <h2>Welcome to React</h2>
-        </div>
-        <WatchListContainer />
-        <AddWatchContainer/>
+let App = () => {
+  return (
+    <div className="App">
+      <div className="App-header">
+        <h2>Welcome to React</h2>
       </div>
-    );
-  }
+      <WatchListContainer />
+      <AddWatchContainer />
+    </div>
+  );
 }
 
 export default App;
