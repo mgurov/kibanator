010258b8ca4719b79e5034e571b2217cedb1b3a2
Mykola Gurov <ngurov@gmail.com>
2017-08-08 21:09:30 +0200

    cleanup

--- src/App.js
+++ src/App.js
@@ -2,51 +2,8 @@ import React, { Component } from 'react';
 import './App.css';
 import AddWatchContainer from './components/AddWatchContainer.js'
 import WatchListContainer from './components/WatchListContainer.js'
-import _ from 'lodash'
 
 class App extends Component {
-  constructor(props) {
-    super(props)
-    this.state = { 
-        watches: this.getStoreWatches(),
-        showModal: false 
-    }
-    this.onAddWatch = _.bind(this.onAddWatch, this)
-    this.onDialogClose = _.bind(this.onDialogClose, this)
-    this.onEditClick = (watch) => {
-      this.setState({showModal: watch})
-    }
-  }
-
-  componentDidMount() {
-    let that = this;
-    this.unsubscribe = this.props.store.subscribe(() => {
-      window.setTimeout(() => {
-        that.setState(_.assign({}, that.state, {watches: that.getStoreWatches()}))
-      })
-      
-    })
-  }
-
-  componentWillUnmount() {
-    if (this.unsubscribe) {
-      this.unsubscribe()
-      this.unsubscribe = null
-    }
-  }
-
-  getStoreWatches() {
-    return this.props.store.getState().watches
-  }
-
-  onAddWatch() {
-    this.setState({ showModal: {text : ""}})
-  }
-
-  onDialogClose(value) {
-    this.setState({showModal: false})
-  }
-
   render() {
     return (
       <div className="App">
