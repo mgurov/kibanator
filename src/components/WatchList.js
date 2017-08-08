8773a52197f92c550ca903729eac33260260334b
Mykola Gurov <ngurov@gmail.com>
2017-08-08 20:53:08 +0200

    let's say it worked

--- src/components/WatchList.js
+++ src/components/WatchList.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Watch from './Watch'
 import PropTypes from 'prop-types';
+import EditWatchForm from './EditWatchForm'
 
 class WatchList extends Component {
     constructor(props) {
@@ -8,14 +9,42 @@ class WatchList extends Component {
         this.state = {
             editing : false
         }
+        let that = this
         this.onEditClick = (watch) => {
-            console.log('Clocked on ', watch)
+            that.setState({editing: watch})
+        }
+        this.onClose = () => {
+            that.setState({editing: false})
+        }
+        this.onSave = (text) => {
+            that.props.onWatchEdited(
+                that.state.editing.id,
+                text
+            )
+            that.onClose()
         }
     }
 
     render() {
         var watches = this.props.watches.map((k, v) => (<Watch key={'item_' + k.id} name={k.text} onClick={() => this.onEditClick(k)} />))
-        return (<div>{watches}</div>)
+
+        let edit;
+        if (!!this.state.editing) {
+            edit =             <EditWatchForm 
+                visible={!!this.state.editing}
+                text={this.state.editing.text}
+                onClose={this.onClose}
+                onSave={this.onSave}
+                />
+
+        } else {
+            edit = null;
+        }
+        
+        return (<div>
+            {watches}
+            {edit}
+        </div>)
     }
 }
 
