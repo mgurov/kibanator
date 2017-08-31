9c7847b4d2df15daa77e80514b42bf9b0ce42a10
Mykola Gurov <ngurov@gmail.com>
2017-08-31 22:37:55 +0200

    ditto

--- src/components/datalist/SelectTimeRange.js
+++ src/components/datalist/SelectTimeRange.js
@@ -3,16 +3,11 @@ import {ButtonGroup, Button} from 'react-bootstrap'
 import _ from 'lodash'
 
 export function SelectTimeRange(props) {
-    function onClickF(st) {
-        return function () {
-            props.onSelected(st)
-        }
-    }
     return <ButtonGroup title="Sync">
     {
         _.map(props.options, st => <Button 
             key={st.name} 
-            onClick={onClickF(st)}
+            onClick={() => props.onSelected(st)}
             >{st.name}</Button>)
     }
     </ButtonGroup>
