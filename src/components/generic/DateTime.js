cdd23822bf4477290b324edb9ab148363dcc712e
Mykola Gurov <ngurov@gmail.com>
2017-09-04 21:37:13 +0200

    first and last if missing are -

--- src/components/generic/DateTime.js
+++ src/components/generic/DateTime.js
@@ -1,6 +1,9 @@
 import React from 'react'
 
 export default function DateTime({ value, ...theRest }) {
+    if (!value) {
+        return <span>-</span>
+    }
     var date = new Date(value);
     return (<span title={value} {...theRest}>{date.toLocaleDateString() + " " + date.toLocaleTimeString()}</span>)
 }
