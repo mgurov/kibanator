0d262c2579723728a2d7404e44830e213eb334fc
Mykola Gurov <ngurov@gmail.com>
2018-04-02 21:22:55 +0200

    nasty testiness

--- src/domain/search.js
+++ src/domain/search.js
@@ -1,23 +1,32 @@
 var makeSearch = function ({serviceName, from, to=new Date(), config}) {
+
+
+    let filters = [
+        {
+            "range": {
+            [config.timeField]: {
+                "gte": from.getTime(),
+                "lte": to.getTime(),
+                "format": "epoch_millis"
+            }
+            }
+        }
+    ]
+
+    if (config.serviceField && serviceName) {
+        filters.push({term: {[config.serviceField]: serviceName.toLowerCase()}})
+    }
+    if (config.levelField && config.levelValue) {
+        filters.push({term: {[config.levelField]: config.levelValue.toLowerCase()}})
+    }
+
     serviceName = serviceName || ''
     return {
         "query": {
             "constant_score": {
                 "filter": {
                     "bool": {
-                        "must": [
-                            {term: {[config.serviceField]: serviceName.toLowerCase()}},
-                            {term: {[config.levelField]: config.levelValue.toLowerCase()}},
-                            {
-                                "range": {
-                                [config.timeField]: {
-                                    "gte": from.getTime(),
-                                    "lte": to.getTime(),
-                                    "format": "epoch_millis"
-                                }
-                                }
-                            }
-                        ],
+                        "must": filters,
                         "must_not": []
                     }
                 }
--- src/domain/search.test.js
+++ src/domain/search.test.js
@@ -5,13 +5,11 @@ test('should filter on the_service when asked so', () => {
     var serviceName = "the_service"
     let actual = makeSearch({serviceName, from: new Date(), config:{serviceField:'Data.app'}});
     
-    const mustPath = 'query.filtered.filter.bool.must';
+    const mustPath = 'query.constant_score.filter.bool.must';
     expect(actual).toHaveProperty(mustPath);
     let must = _(actual).get(mustPath);
 
-    var dataAppSearchFun = _.bind(_.get, _, _, ['query', 'match', 'Data.app']);
+    var dataAppSearchFun = _.bind(_.get, _, _, ['term', 'Data.app']);
     var dataAppFilter = _(must).map(dataAppSearchFun).find();
-    expect(dataAppFilter).toEqual(expect.objectContaining({
-        "query": serviceName
-    }));
+    expect(dataAppFilter).toEqual(serviceName);
 });
