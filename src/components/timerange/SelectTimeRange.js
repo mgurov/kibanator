28c7ea94d54d9c15f31b3ff4a552ae181f1b2584
Mykola Gurov <ngurov@gmail.com>
2018-06-03 16:43:01 +0200

    range button test class

--- src/components/timerange/SelectTimeRange.js
+++ src/components/timerange/SelectTimeRange.js
@@ -11,6 +11,7 @@ export function SelectTimeRange(props) {
     let options = _.map(props.options, st => <Button className="preselected-time-range"
         key={st.name} 
         data-test-id={`range-button-${st.name}`}
+        data-test-class="range-button"
         onClick={() => props.onSelected(st)}
         >{st.name}</Button>)
 
