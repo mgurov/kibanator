f6e4acfa64631abc137e1d30bd221d10045f2729
Mykola Gurov <ngurov@gmail.com>
2018-07-15 12:49:22 +0200

    back ty cypress master

--- scripts/cypress.sh
+++ scripts/cypress.sh
@@ -3,5 +3,5 @@ REACT_APP_INTERVAL=500 yarn build
 docker network create cypress-run || echo joining existing network
 docker rm -f kibanator-sut || echo no container no problem
 docker run --name kibanator-sut -d --network cypress-run --net-alias kibanator -v $(pwd)/build:/srv/http/ui pierrezemb/gostatic --fallback=/ui/index.html
-docker run -it --rm --network cypress-run mgurov/kibanator-cypress test
+docker run -it --rm --network cypress-run mgurov/kibanator-cypress master
 docker rm -f kibanator-sut
