b902e8c54078bfcc16638062a4460cebbeba9f67
Mykola Gurov <ngurov@gmail.com>
2018-07-17 09:04:47 +0200

    lodash.map for resilience

--- src/components/watch/FilterList.js
+++ src/components/watch/FilterList.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import {ListGroup} from 'react-bootstrap'
 import { JsonPre } from '../generic/JsonToggle';
+import _ from 'lodash'
 
 export default function FilterList({value, onRemove}) {
     return (
       <ListGroup>
           {
-              value.map(v => <li className="list-group-item" key={v.key}>
+              _.map(value, v => <li className="list-group-item" key={v.key}>
                     <h4 className="list-group-item-heading">{v.key}{' '}
                         <button onClick={()=>onRemove(v.key)} className="btn">
                             <span className="glyphicon glyphicon-remove" />
