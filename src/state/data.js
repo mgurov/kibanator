ebf057370779a98a70c8a943c9bfdf0cdd73a4c8
Mykola Gurov <ngurov@gmail.com>
2018-07-18 22:21:20 +0200

    no need selectedData

--- src/state/data.js
+++ src/state/data.js
@@ -1,20 +1,3 @@
-import {emptyState} from '../reducers/data'
-
 export function watchIndexData(state, watchIndex) {
     return state.watches.data[watchIndex + ""]
-}
-
-export function selectedData(state, fallbackEmptyState) {
-    const watchIndex = state.view.watchIndex
-    if (undefined !== watchIndex) {
-        let theData = watchIndexData(state, watchIndex + "")
-        if (undefined !== theData) {
-            return theData
-        }
-    }
-    if (fallbackEmptyState) {
-        return emptyState
-    } else {
-        return undefined
-    }
 }
